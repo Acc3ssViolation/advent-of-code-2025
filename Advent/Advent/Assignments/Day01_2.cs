b79120e99f2bad810e8b99bed55c0698a9152b5d
Acc3ss Violation <wrdbrookhuis@gmail.com>
2025-12-01 20:16:40 +0100

    Day 1.2
    It's dump but it works

---
+++ Advent/Advent/Assignments/Day01_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Advent.Assignments
+{
+    internal class Day01_2 : IAssignment
+    {
+        public string Run(IReadOnlyList<string> input)
+        {
+            const int DialSize = 100;
+            var dial = 50;
+            var zeroCount = 0;
+            foreach (var item in input)
+            {
+                var num = int.Parse(item[1..]);
+                var fullCircles = num / DialSize;
+                zeroCount += fullCircles;
+
+                num %= DialSize;
+
+                var dir = item[0] == 'L' ? DialSize - 1 : 1;
+
+                // fuck it i'm tired
+                var asf = 0;
+                for (var click = 0; click < num; click++)
+                {
+                    dial = (dial + dir ) % DialSize;
+
+                    if (dial == 0)
+                        asf++;
+                }
+
+                //Logger.DebugLine($"The dial is rotated {item} to the point at {dial}; during this rotation, it points at 0 {asf}");
+
+                zeroCount += asf;
+            }
+
+            return zeroCount.ToString();
+        }
+    }
+}
