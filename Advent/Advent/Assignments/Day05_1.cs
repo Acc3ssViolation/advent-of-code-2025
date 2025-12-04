79a90949a9321699766125f80b72dd6b18844739
Acc3ss Violation <wrdbrookhuis@gmail.com>
2025-12-04 23:06:48 +0100

    Add scaffolding for day 5

---
+++ Advent/Advent/Assignments/Day05_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Assignments
+{
+    internal class Day05_1 : IAssignment
+    {
+        public string Run(IReadOnlyList<string> input)
+        {
+            var result = 0;
+            return result.ToString();
+        }
+    }
+}
