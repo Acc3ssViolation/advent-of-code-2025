8467352272dda2191aa8bbf6beebd4a4d349318a
Acc3ss Violation <wrdbrookhuis@gmail.com>
2025-12-09 22:18:56 +0100

    Add a recursive flood fill implementation that triggers stack overflows

--- Advent/Advent/FloodFillExtensions.cs
+++ Advent/Advent/FloodFillExtensions.cs
@@ -31,5 +31,27 @@ namespace Advent
                     queue.Enqueue(point with { y = point.y + 1 });
             }
         }
+
+        public static void FloodFillRecursive(this CompactGrid grid, Point start, byte value)
+        {
+            static void FloodFillRecursive(CompactGrid grid, Point point, byte replaceValue, byte value)
+            {
+                if (grid[point] != replaceValue)
+                    return;
+                grid[point] = value;
+
+                if (point.x > 0)
+                    FloodFillRecursive(grid, point with { x = point.x - 1 }, replaceValue, value);
+                if (point.x < grid.Width - 1)
+                    FloodFillRecursive(grid, point with { x = point.x + 1 }, replaceValue, value);
+                if (point.y > 0)
+                    FloodFillRecursive(grid, point with { y = point.y - 1 }, replaceValue, value);
+                if (point.y < grid.Height - 1)
+                    FloodFillRecursive(grid, point with { y = point.y + 1 }, replaceValue, value);
+            }
+
+            var replaceValue = grid[start];
+            FloodFillRecursive(grid, start, replaceValue, value);
+        }
     }
 }
